615a0d08165e61658eef7b8af245232775383e8d
NaDa007 <110329129+NaDa007@users.noreply.github.com>
2022-12-10 21:56:25 +0900

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,13 +1,15 @@
 const titleText = document.querySelector('.title');
-const Christmas = new Audio('../data/christmas.mp3')
-
+const Christmas = new Audio('./data/christmas.mp3')
+console.log("Audio.js Loading!");
 
 function play() {
+    console.log("Play Starting!");
     Christmas.volume = 0;
     Christmas.play();
     Christmas.loop = true;
 
-    setTimeout(function() { Christmas.volume = 0.7; }, 1000)
+    setTimeout(function() { Christmas.volume = 0.7; }, 1500)
+    console.log("Play Started!");
 }
 
 
@@ -15,10 +17,12 @@ function getDate() {
     const date = new Date();
     const month = date.getMonth()  + 1;
     const day = date.getDate();
+    console.log("Date Update!");
 
     if (!(titleText.innerText == "Christmas Is Coming Soon! 🔔") || (titleText.innerText == "🎁 Today Is Christmas! 🎄")) {
         const player = document.querySelector('.player');
         Christmas.pause();
+        console.log("Christmas Stop!");
         
     } 
 }
