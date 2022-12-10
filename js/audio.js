94c46bddbf9e276bd83eb79be1bb386f0606e6c7
NaDa007 <110329129+NaDa007@users.noreply.github.com>
2022-12-10 21:46:48 +0900

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,21 +1,13 @@
 const titleText = document.querySelector('.title');
-
+const Christmas = new Audio('./data/christmas.mp3')
 
 
 function play() {
-    var player = document.createElement("iframe");
-        player.classList.add('player');
-        player.src = "https://www.youtube.com/embed/QlQCGUK8tN4?autoplay=1&mute=1&loop=1&start=001&playlist=QlQCGUK8tN4&playsinline=1&enablejsapi=1";
-        player.width = "560";
-        player.height = "315";
-        player.title = "YouTube video player";
-        player.frameborder = "0";
-        player.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-        player.allowFullscreen;
+    Christmas.volume = 0;
+    Christmas.play();
+    Christmas.loop = true;
 
-        document.body.appendChild(player);
-        const playerEl = document.querySelector('.player');
-        playerEl.src = "https://www.youtube.com/embed/QlQCGUK8tN4?autoplay=1&mute=0&loop=1&start=001&playlist=QlQCGUK8tN4&playsinline=1&enablejsapi=1";
+    setTimeout(function() { Christmas.volume = 0.7; }, 1000)
 }
 
 
@@ -26,7 +18,7 @@ function getDate() {
 
     if (!(titleText.innerText == "Christmas Is Coming Soon! 🔔") || (titleText.innerText == "🎁 Today Is Christmas! 🎄")) {
         const player = document.querySelector('.player');
-        player.remove();
+        Christmas.pause();
         
     } 
 }
