59d926a0e47cfbef430044e11d715ddf53721f9f
NaDa007 <110329129+NaDa007@users.noreply.github.com>
2022-12-11 20:42:47 +0900

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,29 +1,29 @@
+const Christmas = new Audio('./data/christmas.mp3');
 const titleText = document.querySelector('.title');
-const Christmas = new Audio('./data/christmas.mp3')
+const musicBtn = document.querySelector('.musicBtn');
 
-
+console.log(musicBtn.src);
 function play() {
-    Christmas.volume = 0.7;
-    Christmas.muted = true;
-    Christmas.loop = true;
-    Christmas.play();
-
-    setTimeout(function() { Christmas.muted = false; }, 1000)
+    console.log("musicBtn: Submit!")
+    if (String(musicBtn.src).includes('playBtn.png')) {
+        Christmas.play();
+        musicBtn.src = './data/pauseBtn.png';
+    } else if (String(musicBtn.src).includes('pauseBtn.png')) {
+        Christmas.pause();
+        musicBtn.src = './data/playBtn.png';
+    }
+    
 }
 
 
 function getDate() {
-    const date = new Date();
-    const month = date.getMonth()  + 1;
-    const day = date.getDate();
-
     if (!(titleText.innerText == "Christmas Is Coming Soon! 🔔") || (titleText.innerText == "🎁 Today Is Christmas! 🎄")) {
-        Christmas.pause();
-        
+        Christmas.remove()
     } 
 }
 
+musicBtn.addEventListener('click', play);
 
-play();
+
 getDate();
 setInterval(getDate, 1000);
